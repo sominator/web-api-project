49f0600fcb4aeae1e6f614a169a6d6f8e263e90b
andersj <anders.jenbo@valitor.com>
2020-03-02 20:18:53 +0100

    php artisan make:controller QuestController --api --model=Quest

---
+++ server-laravel/app/Http/Controllers/QuestController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quest;
+use Illuminate\Http\Request;
+
+class QuestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Quest  $quest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quest $quest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Quest  $quest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Quest $quest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Quest  $quest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Quest $quest)
+    {
+        //
+    }
+}
